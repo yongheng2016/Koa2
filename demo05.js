d10af894118a2549c95d827bffe182f42cb6fc20
yongheng <1074827582@qq.com>
2018-05-08 15:14:51 +0800

    路由层级

--- demo05.js
+++ demo05.js
@@ -2,15 +2,48 @@ const Koa = require('koa')
 const Router = require('koa-router')
 
 const app = new Koa()
-const router = new Router()
+//父级路由
+const router = new Router({
+    prefix: '/good'
+})
+//home路由
+const home = new Router()
+//page路由
+const page = new Router()
 
+home
+    .get('/test', async (ctx) => {
+        ctx.body = 'home路由下'
+    })
+    .get('/todo', async (ctx) => {
+        ctx.body = 'home -- todo'
+    })
+
+page
+    .get('/test', async (ctx) => {
+        ctx.body = 'page路由下'
+    })
+    .get('/todo', async (ctx) => {
+        ctx.body = 'page -- todo'
+    })
 router
     .get('/', (ctx, next) => {
-        ctx.body = 'hello'
+        let html = `
+            <ul><a href="/good">good/index</a></ul>
+            <ul><a href="/good/todo">good/todo</a></ul>
+            <ul><a href="/good/home/test">/good/home/test</a></ul>
+            <ul><a href="/good/home/todo">/good/home/todo</a></ul>
+            <ul><a href="/good/page/test">/good/page/test</a></ul>
+            <ul><a href="/good/page/todo">/good/page/todo</a></ul>
+        `
+        ctx.body = html
     })
     .get('/todo', (ctx, next) => {
         ctx.body = 'todo'
     })
+
+router.use('/home', home.routes(), home.allowedMethods())
+router.use('/page', page.routes(), page.allowedMethods())
 
 app.use(router.routes())
     .use(router.allowedMethods())

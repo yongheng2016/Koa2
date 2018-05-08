3d8fc81c2b30efe219b57c2a6713de5fafc89546
yongheng <1074827582@qq.com>
2018-05-08 15:59:34 +0800

    模板引擎路径查找 模板引擎具体引用 模板引擎参数传递

--- demo08.js
+++ demo08.js
@@ -3,13 +3,14 @@ const Views = require('koa-views')
 const path = require('path')
 const app = new Koa()
 
- //ejs模板引用
+ //ejs模板引用路径
 app.use(Views(path.join(__dirname, './views'), {
     extension: 'ejs' 
 }))
 
 app.use(async ctx=> {
     let title = 'hello world'
+    //通过文件名(eg: index => index.ejs)来引用模板并传参
     await ctx.render('index', {title})
 })
 

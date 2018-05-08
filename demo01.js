87959d0d5d2ecf497d6213df01b0b927b073c1bc
yongheng <1074827582@qq.com>
2018-05-08 13:56:57 +0800

    koa-bodyparser 中间件

--- demo01.js
+++ demo01.js
@@ -3,8 +3,11 @@ const app = new Koa()
 app.use(async ctx => {
     let url = ctx.url
     let req = ctx.request
+    let ctx_querystring = ctx.querystring
     console.log('ctx', ctx)
-    ctx.body = ctx
+    ctx.body = {
+        ctx_querystring
+    }
 })
 app.listen(3000, () => {
     console.log('server is start at port 3000')
---
+++ demo03.js
@@ -0,0 +1,54 @@
+const Koa = require('koa')
+const app = new Koa()
+const bodyparser = require('koa-bodyparser')
+app.use(bodyparser())
+function parseData(ctx) {
+    return new Promise((resolve, reject) => {
+        try {
+            let postData = ''
+            ctx.req.addListener('data', (data) => {
+                postData += data
+            })
+            ctx.req.on('end', () => {
+                resolve(jsonParse(postData))
+            })
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
+function jsonParse(qeury) {
+    let params = {}
+    let paramsList = qeury.split('&')
+    for(let [index, val] of paramsList.entries()) {
+        let itemList = val.split('=')
+        params[itemList[0]] = decodeURIComponent(itemList[1])
+    }
+    return params
+}
+
+app.use(async ctx => {
+    if (ctx.url === '/' && ctx.method === 'GET') {
+        let html = `
+            <h1>Koa form</h1>
+            <form method="POST" action="/">
+                <p>userName</p>
+                <input type="text" name="name"/>
+                <p>age</p>
+                <input type="text" name="age"/>
+                <input type="submit"/>
+            </form>
+        `
+        ctx.body = html
+    } else if (ctx.url === '/' && ctx.method === 'POST') {
+        // let postData = await parseData(ctx)
+        let postData = ctx.request.body
+        ctx.body = postData
+    } else {
+        ctx.body = '404'
+    }
+})
+app.listen(3000, () => {
+    console.log('server started at port at 3000')
+})
